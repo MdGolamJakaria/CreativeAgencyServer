08092e9e177ac47916c7db25ad921041db132357
MdGolamJakaria <jakaria3335@gmail.com>
2020-11-06 05:32:52 +0600

    updateed

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const port = 5000
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const fs = require('fs-extra')
+const fileUpload = require('express-fileupload')
 require('dotenv').config()
 app.use(bodyParser.json());
 app.use(cors())
+app.use(express.static('images'));
+app.use(fileUpload());
 
 const USER_NAME = process.env.DB_USER;
 const USER_PASS = process.env.DB_PASS;
@@ -15,6 +19,7 @@ const DB_NAME = process.env.DB_NAME;
 const uri = `mongodb+srv://${USER_NAME}:${USER_PASS}@cluster0.sfno3.gcp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+
     const adminsCollection = client.db("CreativeAgency").collection("admins");
     app.post('/addAdmin', (req, res) => {
         const admin = req.body;
@@ -27,19 +32,71 @@ client.connect(err => {
 
     const serviceCollection = client.db("CreativeAgency").collection("services");
     app.post('/addService', (req, res) => {
-        const service = req.body;
-        serviceCollection.insertOne(service)
+        const file = req.files.file;
+        const title = req.body.title;
+        const description = req.body.description;
+
+        const filePath = `${__dirname}/images/${file.name}`
+        file.mv(filePath, err => {
+            if (err) {
+                console.log(err)
+                return res.status(500).send({ msg: 'Upload failed' })
+            }
+            return res.send({ name: file.name, path: `/${file.name}` })
+        })
+        const newImage = fs.readFileSync(filePath);
+        const encImg = newImage.toString('base64')
+        var icon = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        serviceCollection.insertOne({ title, description, icon })
             .then(result => {
                 console.log(result)
                 res.send(result.insertedCount > 0)
             })
     })
+    app.get("/services", (req, res) => {
+        serviceCollection.find({})
+            .toArray((error, documents) => {
+                res.send(documents)
+            })
+    })
+
+
+
     const orderCollection = client.db("CreativeAgency").collection("orders");
     app.post('/addOrder', (req, res) => {
-        const order = req.body;
-        orderCollection.insertOne(order)
+        const file = req.files.file;
+        const user = req.body.user;
+        const details = req.body.details;
+        const price = req.body.price;
+        const service = req.body.service;
+
+        const filePath = `${__dirname}/images/${file.name}`
+        file.mv(filePath, err => {
+            if (err) {
+                console.log(err)
+                return res.status(500).send({ msg: 'Upload failed' })
+            }
+            return res.send({ name: file.name, path: `/${file.name}` })
+        })
+        const newImage = fs.readFileSync(filePath);
+        const encImg = newImage.toString('base64')
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+        orderCollection.insertOne({ user, details, price, service, image })
             .then(result => {
-                console.log(result)
+                fs.remove(filePath, error => {
+                    if (error) {
+                        console.log(error)
+                    }
+                })
                 res.send(result.insertedCount > 0)
             })
     })
