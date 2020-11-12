ede110845f30d3cb8485462abe8d643fdac1f84a
MdGolamJakaria <jakaria3335@gmail.com>
2020-11-12 11:33:50 +0600

    Done

--- index.js
+++ index.js
@@ -35,29 +35,20 @@ client.connect(err => {
         const file = req.files.file;
         const title = req.body.title;
         const description = req.body.description;
-
-        const filePath = `${__dirname}/images/${file.name}`
-        file.mv(filePath, err => {
-            if (err) {
-                console.log(err)
-                return res.status(500).send({ msg: 'Upload failed' })
-            }
-            return res.send({ name: file.name, path: `/${file.name}` })
-        })
-        const newImage = fs.readFileSync(filePath);
+        const newImage = file.data;
         const encImg = newImage.toString('base64')
         var icon = {
             contentType: file.mimetype,
             size: file.size,
             img: Buffer.from(encImg, 'base64')
         };
-
         serviceCollection.insertOne({ title, description, icon })
             .then(result => {
                 console.log(result)
                 res.send(result.insertedCount > 0)
             })
     })
+
     app.get("/services", (req, res) => {
         serviceCollection.find({})
             .toArray((error, documents) => {
@@ -67,39 +58,42 @@ client.connect(err => {
 
 
 
+
     const orderCollection = client.db("CreativeAgency").collection("orders");
     app.post('/addOrder', (req, res) => {
         const file = req.files.file;
         const user = req.body.user;
+        const email = req.body.email;
         const details = req.body.details;
         const price = req.body.price;
         const service = req.body.service;
-
-        const filePath = `${__dirname}/images/${file.name}`
-        file.mv(filePath, err => {
-            if (err) {
-                console.log(err)
-                return res.status(500).send({ msg: 'Upload failed' })
-            }
-            return res.send({ name: file.name, path: `/${file.name}` })
-        })
-        const newImage = fs.readFileSync(filePath);
+        const newImage = file.data;
         const encImg = newImage.toString('base64')
         var image = {
             contentType: file.mimetype,
             size: file.size,
             img: Buffer.from(encImg, 'base64')
         };
-        orderCollection.insertOne({ user, details, price, service, image })
+        orderCollection.insertOne({ user, email, details, price, service, image })
             .then(result => {
-                fs.remove(filePath, error => {
-                    if (error) {
-                        console.log(error)
-                    }
-                })
                 res.send(result.insertedCount > 0)
             })
     })
+
+    app.post('/userMail', (req, res) => {
+        const email = req.body.email;
+        console.log(email)
+        orderCollection.find({ email : email })
+            .toArray((err, orders) => {
+                res.send(orders.length> 0);
+            })
+    })
+    // app.get("/orderListByEmail", (req, res) => {
+    //     orderCollection.find({})
+    //         .toArray((error, documents) => {
+    //             res.send(documents)
+    //         })
+    // })
 
     const reviewCollection = client.db("CreativeAgency").collection("review");
     app.post('/addReview', (req, res) => {
@@ -129,6 +123,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
